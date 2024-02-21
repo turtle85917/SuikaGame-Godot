ba46bea0a14a773f1b8279102d3d7669d6888506
Pulotodom <61219276+turtle85917@users.noreply.github.com>
2024-02-21 21:54:09 +0900

    feat: implement main scene

--- Scripts/GameOverControl.gd
+++ Scripts/GameOverControl.gd
@@ -1,14 +1,19 @@
 extends Control
 
 @onready var restartButton:TextureButton = $"Restart Button";
+@onready var backButton:TextureButton = $"Back Button";
 @onready var quitButton:TextureButton = $"Quit Button";
 
 func _ready():
 	restartButton.connect("pressed", _on_restart_button_pressed);
+	backButton.connect("pressed", _on_back_button_pressed);
 	quitButton.connect("pressed", _on_quit_button_pressed);
 
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene();
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn");
 
 func _on_quit_button_pressed():
 	get_tree().quit();
---
+++ Scripts/Main.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var startButton = $"CanvasLayer/Control/Start Button";
+
+func _ready():
+	startButton.connect("pressed", _on_start_button_pressed);
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn");
